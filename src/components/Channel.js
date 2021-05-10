e9922ef115755e332c135fcd58f47fc2abb2b55b
Monu Shukla <46845822+m26unkwn@users.noreply.github.com>
2021-05-10 13:07:37 +0530

    Updated the title
    Changed some typo

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -53,10 +53,10 @@ const Channel = ({ user = null }) => {
           <div className='border-b dark:border-gray-600 border-gray-200 py-8 mb-4'>
             <div className='font-bold text-3xl text-center'>
               <p className='mb-1'>Welcome to</p>
-              <p className='mb-3'>React TrashStore</p>
+              <p className='mb-3'>TrashChat</p>
             </div>
             <p className='text-gray-400 text-center'>
-              Its another galaxy of trashChat.
+              Its another galaxy of trash Universe.
             </p>
           </div>
           <ul>
